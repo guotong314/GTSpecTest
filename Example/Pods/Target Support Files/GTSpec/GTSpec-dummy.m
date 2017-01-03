d419af6fc57f5682317eaa23e2291031de517e99
郭通 <guotong314@gmail.com>
2017-01-03 13:02:30 +0800

    添加 打电话发短信

--- Example/Pods/Target Support Files/GTSpec/GTSpec-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GTSpec : NSObject
-@end
-@implementation PodsDummy_GTSpec
-@end
--- Example/Pods/Target Support Files/GTSpec/GTSpec-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double GTSpecVersionNumber;
-FOUNDATION_EXPORT const unsigned char GTSpecVersionString[];
-
